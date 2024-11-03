75cff7cff06e3e1365101976ac489a2944a88e62
Mariano Koci <mariano.koci@seciu.edu.uy>
2024-11-02 22:37:53 -0300

    separacion de repositorios

--- flowboard_backend/dist/controllers/TeamController.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from "express";
-export declare class TeamController {
-    static findMemberByEmail: (req: Request, res: Response) => Promise<void>;
-    static getProjectTeam: (req: Request, res: Response) => Promise<void>;
-    static addUserById: (req: Request, res: Response) => Promise<void>;
-    static removeUserById: (req: Request, res: Response) => Promise<void>;
-}
--- flowboard_backend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import app from './server'
-import colors from 'colors'
-
-
-const PORT = process.env.PORT || 4000
-
-app.listen(PORT, () => {
-    console.log(colors.white.bgMagenta.bold(`Server is running on port ${PORT}`))
-})
